8a22168062e3a58c3ae0e2608b52c99ef778d96b
PENG <chengyuan.23@intl.zju.edu.cn>
2024-03-07 09:11:22 +0800

    practical

---
+++ Pactical_4/variables.py
@@ -0,0 +1,18 @@
+a=40
+b=36
+c=30
+d=a-b
+e=b-c
+if d-e<0:
+    print("a combination of running and strength exercises is more effective")
+elif d-e==0:
+    print("running is as effective as a combination of running and strength exercises")
+else:
+    print:("running is more effective")
+
+X=True
+Y=False
+W=X or Y
+print(W)
+
+result: True
